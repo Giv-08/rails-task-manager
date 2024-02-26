50bf436ec7124622c38b127da25ae9e011145554
chutima puthachon <chutima.p8@gmail.com>
2024-02-26 15:37:21 +1100

    submit task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/task', to: 'tasks#create'
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new' # display form
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post '/tasks', to: 'tasks#create' # add details
+
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  patch '/tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
